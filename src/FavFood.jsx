5cbc6d144838ff394e9239b90c4555750577737c
ahmed <ahmed.elmohanned@gmail.com>
2025-01-13 00:51:34 +0200

    more react fiddling/learning

---
+++ src/FavFood.jsx
@@ -0,0 +1,12 @@
+import shawarma from "./assets/shawarma.jpg";
+
+function FavFood() {
+  return (
+    <>
+      <h1>This is my Favourtie fooood!</h1>
+      <img src={shawarma} alt="" />
+    </>
+  );
+}
+
+export { FavFood };
